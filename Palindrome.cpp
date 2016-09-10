a7ece112bc45462b57aa4ba16624f409d524b83b
JHOWELL <atlas3141@gmail.com>
2016-09-09 20:01:00 -0700

    "Takes in a string"

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
-using namespace std
+using namespace std;
 
 int main(){
-
+  char inputstr[80];
+  char cleanstr[80];
+  cin.get(inputstr,80);
+  cout << inputstr << endl;
 }

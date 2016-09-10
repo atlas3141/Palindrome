17f9bad7977d439eaf6763543e01c136ba3dbf8e
JHOWELL <atlas3141@gmail.com>
2016-09-09 20:18:24 -0700

    "Checks if its a palindrome and tells you if it is"

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int main(){
-  char inputstr[80];
+  char inputstr[80]; 
   char cleanstr[80];
-  cin.get(inputstr,80);
-  cout << inputstr << endl;
+  cin.get(inputstr,80); //get the string
   int j = 0;
-  for (int i = 0; i < 80; i++){
+  for (int i = 0; i < 80; i++){ //removes punctuation and junk
     if (isalpha(inputstr[i])){
       cleanstr[j] = tolower(inputstr[i]);
       j++;
@@ -18,5 +17,16 @@ int main(){
       cleanstr[j] = '\0';
     }
   }
-  cout << cleanstr << endl;
+  bool isPalindrome = true; // checks to see if its a palindrome
+  for (int i = 0; i < j/2; i++){
+    if (cleanstr[i] != cleanstr[j-1-i]){
+      isPalindrome = false;
+    }
+  }
+  if (isPalindrome){ //tells you if its a palindrome
+    cout << inputstr << " is a palindrome" << endl;
+  }
+  else{
+    cout << inputstr << " is not a palindrome" << endl;
+  }
 }

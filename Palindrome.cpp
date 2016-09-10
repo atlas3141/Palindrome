0618b42a680e37d78613f046ceb0aa77b2714ea1
JHOWELL <atlas3141@gmail.com>
2016-09-09 20:08:51 -0700

    Cleans up the string to get rid of spaces and punctuation and numbers

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -7,4 +7,16 @@ int main(){
   char cleanstr[80];
   cin.get(inputstr,80);
   cout << inputstr << endl;
+  int j = 0;
+  for (int i = 0; i < 80; i++){
+    if (isalpha(inputstr[i])){
+      cleanstr[j] = tolower(inputstr[i]);
+      j++;
+    }
+    if (inputstr[i+1] == '\0'){
+      i=81;
+      cleanstr[j] = '\0';
+    }
+  }
+  cout << cleanstr << endl;
 }
